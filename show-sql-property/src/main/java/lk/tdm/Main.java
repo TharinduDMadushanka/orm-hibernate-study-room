cbe438494b7eb2dddd65938f53bcc545f7d7c316
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-03 10:51:28 +0530

    show sql queries

---
+++ show-sql-property/src/main/java/lk/tdm/Main.java
@@ -0,0 +1,31 @@
+package lk.tdm;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Alien a1 = new Alien();
+        a1.setId(2);
+        a1.setName("Madu");
+        a1.setColor("Blue");
+
+
+        Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        session.save(a1);
+//        session.delete(a1);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
