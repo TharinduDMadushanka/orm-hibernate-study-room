53d81325b9de80a32126f73fa884dcc8b4e77e94
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-03 09:56:46 +0530

    hibernate basics

--- hibernate-01/src/main/java/lk/tdm/Alien.java
+++ hibernate-01/src/main/java/lk/tdm/Alien.java
@@ -39,3 +39,16 @@ public class Alien {
         this.color = color;
     }
 }
+
+/**
+
+ * @Entity:  * This annotation marks the class as a JPA entity, meaning it is mapped to a database table.
+            * Hibernate will use this class to perform CRUD operations on the corresponding table.
+
+ * @Table(name = * "alien"): This specifies the name of the database table that the entity maps to.
+                 * In this case, the table name is alien. If you omit this annotation,
+                 * Hibernate will use the class name as the default table name.
+
+ * @Id: * This annotation designates the id field as the primary key for the entity.
+        * The primary key uniquely identifies each record in the table.
+ */
